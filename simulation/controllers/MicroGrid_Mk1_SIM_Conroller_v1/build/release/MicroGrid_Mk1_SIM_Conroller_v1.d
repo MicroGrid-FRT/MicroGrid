5d395628921cd1c763ab25dd46156c85ff2bccbd
MicroGrid-FRT <frtmicrogrid@gmail.com>
2026-06-01 14:13:33 +0200

    Upload Webots simulation files
    MicroGrid.wbt
    TestRobot.proto
    testrobot_drive controller
    MicroGrid_Mk1_SIM_Controller_v1 controller

---
+++ simulation/controllers/MicroGrid_Mk1_SIM_Conroller_v1/build/release/MicroGrid_Mk1_SIM_Conroller_v1.d
@@ -0,0 +1,10 @@
+build/release/MicroGrid_Mk1_SIM_Conroller_v1.o: \
+ MicroGrid_Mk1_SIM_Conroller_v1.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Gyro.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Accelerometer.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Keyboard.hpp
